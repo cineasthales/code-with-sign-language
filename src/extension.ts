73bde367010c48bd6baf74dcc6c5ad9a78d87303
Thales Cezar <28463839+cineasthales@users.noreply.github.com>
2023-06-16 09:15:13 -0300

    dynamic css and js declaration in html

--- src/extension.ts
+++ src/extension.ts
@@ -21,61 +21,69 @@ export function activate(context: vscode.ExtensionContext)
 }
 
 function getWebviewContent(webview: vscode.Webview, uri: vscode.Uri): string
-{
-	const css = [
+{	
+	const allCss = [
 		webview.asWebviewUri(vscode.Uri.joinPath(uri,'src','icons','css','fontawesome.css')),
 		webview.asWebviewUri(vscode.Uri.joinPath(uri,'src','icons','css','regular.css')),
 		webview.asWebviewUri(vscode.Uri.joinPath(uri,'src','icons','css','solid.css')),
 		webview.asWebviewUri(vscode.Uri.joinPath(uri,'src','css','webview.css')),
 	];
 
-	const js = [
+	const allJs = [
 		webview.asWebviewUri(vscode.Uri.joinPath(uri,'src','js','jquery.js')),
 		webview.asWebviewUri(vscode.Uri.joinPath(uri,'src','js','webview.js')),
 	];
 
-	return `
+	let html = `
 	<!DOCTYPE html>
 	<html>
 	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="${css[0]}">
-		<link rel="stylesheet" href="${css[1]}">
-		<link rel="stylesheet" href="${css[2]}">
-		<link rel="stylesheet" href="${css[3]}">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	`;
+
+	for (let css of allCss) {
+		html += `<link rel="stylesheet" href="${css}">`;
+	}
+
+	html += `
 	</head>
 	<body>
-		<main>
-			<div id="videoContainer"></div>
-			<div id="infoContainer">
-				<div id="currentTime"></div>
-				<div id="currentSign"></div>
-				<div id="totalDuration"></div>
-			</div>
-			<div id="playerContainer">
-				<button id="rewind">
-					<i class="fa-solid fa-backward-fast" id="rewindIcon"></i>
-				</button>
-				<button id="backward">
-					<i class="fa-solid fa-backward-step" id="backwardIcon"></i>
-				</button>
-				<button id="playPause">
-					<i class="fa-regular fa-circle-play" id="playPauseIcon"></i>
-				</button>
-				<button id="forward">
-					<i class="fa-solid fa-forward-step" id="forwardIcon"></i>
-				</button>
-				<button id="autoRepeat">
-					<i class="fa-solid fa-repeat" id="autoRepeatIcon"></i>
-				</button>
-			</div>
-		</main>
-		<script src="${js[0]}"></script>
-		<script src="${js[1]}"></script>
+		<div id="videoContainer"></div>
+		<div id="infoContainer">
+			<div id="currentTime"></div>
+			<div id="currentSign"></div>
+			<div id="totalDuration"></div>
+		</div>
+		<div id="playerContainer">
+			<button id="rewind">
+				<i class="fa-solid fa-backward-fast" id="rewindIcon"></i>
+			</button>
+			<button id="backward">
+				<i class="fa-solid fa-backward-step" id="backwardIcon"></i>
+			</button>
+			<button id="playPause">
+				<i class="fa-regular fa-circle-play" id="playPauseIcon"></i>
+			</button>
+			<button id="forward">
+				<i class="fa-solid fa-forward-step" id="forwardIcon"></i>
+			</button>
+			<button id="autoRepeat">
+				<i class="fa-solid fa-repeat" id="autoRepeatIcon"></i>
+			</button>
+		</div>
+  	`;
+
+	for (let js of allJs) {
+		html += `<script src="${js}"></script>`;
+	}
+
+	html += `
 	</body>
 	</html>
 	`;
+
+	return html;
 }
 
 export function deactivate() {}
