61d032a17a5e7250b88f45198294e346434f6faa
Thales Cezar <28463839+cineasthales@users.noreply.github.com>
2024-01-08 09:47:23 -0300

    refactoring translator as a class

--- src/languages/translator.ts
+++ src/languages/translator.ts
@@ -4,166 +4,192 @@ import * as javascriptCategories from './javascript/categories';
 import { ITooltips, ISign, ISignVideos, ICategory, ICategoryVideos } from '../utils/interfaces';
 import { tooltipsIds } from '../utils/constants';
 
-export function getWelcome(signLanguage: string, webview: vscode.Webview, uri: vscode.Uri): ISignVideos[]
+class Translator
 {
-	const videos: ISignVideos[] = [];
-	videos.push({
-		token: '',
-		file: webview.asWebviewUri(vscode.Uri.joinPath(uri,'videos',signLanguage,'welcome.mp4')),
-		info: undefined,
-		examples: undefined,
-	});
-	return videos;
-}
+	signLanguage: string;
+	webview: vscode.Webview;
+	uri: vscode.Uri;
 
-export function getTooltips(signLanguage: string, webview: vscode.Webview, uri: vscode.Uri): ITooltips[]
-{
-	const tooltips: ITooltips[] = [];
-	for (let tooltip of tooltipsIds)
+	constructor(signLanguage: string, webview: vscode.Webview, uri: vscode.Uri): void
 	{
-		tooltips.push({
-			id: tooltip,
-			file: webview.asWebviewUri(vscode.Uri.joinPath(uri,'videos',signLanguage,'tooltip',tooltip+'.mp4')),
+		this.signLanguage = signLanguage;
+		this.webview = webview;
+		this.uri = uri;
+	}
+
+	getWelcome(): ISignVideos[]
+	{
+		const videos: ISignVideos[] = [];
+		videos.push({
+			token: '',
+			file: this.webview.asWebviewUri(
+				vscode.Uri.joinPath(this.uri,'videos',this.signLanguage,'welcome.mp4')
+			),
+			info: undefined,
+			examples: undefined,
 		});
-	}
-	return tooltips;
-}
-
-export function getError(message: string, signLanguage: string, webview: vscode.Webview, uri: vscode.Uri): ISignVideos[]
-{
-	const videos: ISignVideos[] = [];
-	videos.push({
-		token: 'Erro!',
-		file: webview.asWebviewUri(vscode.Uri.joinPath(uri,'videos',signLanguage,'error',message+'.mp4')),
-		info: undefined,
-		examples: undefined,
-	});
-	return videos;
-}
-
-export function readCode(signLanguage: string, editor: vscode.TextEditor, webview: vscode.Webview, uri: vscode.Uri): ISignVideos[]
-{
-	const language: string = editor.document.languageId;
-	const results: ISign[] = [];
-	const videos: ISignVideos[] = [];
-
-	const text: string = sanitizeEditorText(editor);
-
-	switch (language)
-	{
-		case 'javascript':
-			results.push(...javascriptCodeChecker.getResults(text));
-			break;
-		default:
-			return videos;
+		return videos;
 	}
 
-	if (results)
+	getTooltips(): ITooltips[]
 	{
-		for (let result of results)
+		const tooltips: ITooltips[] = [];
+		for (let tooltip of tooltipsIds)
 		{
-			if (result.isCode)
-			{
-				videos.push({
-					token: result.token,
-					file: webview.asWebviewUri(
-						vscode.Uri.joinPath(uri,'videos',signLanguage,'languages',language,'code',result.file+'.mp4')),
-					info: result.info ?
-						webview.asWebviewUri(
-							vscode.Uri.joinPath(uri,'videos',signLanguage,'languages',language,'info',result.info+'.mp4'))
-						: undefined,
-					examples: undefined,
-				});
-			}
-			else
-			{
-				videos.push({
-					token: result.token,
-					file: webview.asWebviewUri(vscode.Uri.joinPath(uri,'videos',signLanguage,'misc',result.token+'.mp4')),
-					info: undefined,
-					examples: undefined,
-				});
-			}
-		}
-	}
-
-	return videos;
-}
-
-export function writeCode(editor: vscode.TextEditor, text: string)
-{
-	const position: vscode.Position = editor.selection.active;
-	editor.edit((builder: vscode.TextEditorEdit) => { builder.insert(position, text); });
-}
-
-export function getCategories(signLanguage: string, currentLanguage: string, editorLanguage: string, webview: vscode.Webview, uri: vscode.Uri): ICategoryVideos[]
-{
-	if (currentLanguage === editorLanguage) {
-		return categories;
-	}
-	
-	const results: ICategory[] = [];
-	const categories: ICategoryVideos[] = [];
-
-	switch (editorLanguage)
-	{
-		case 'javascript':
-			results.push(...javascriptCategories.categories);
-			break;
-		default:
-			return categories;
-	}
-
-	if (results)
-	{
-		for (let result of results)
-		{
-			const categoryVideos: ISignVideos[] = [];
-			
-			for (let sign of result.signs)
-			{
-				categoryVideos.push({
-					token: sign.token,
-					file: webview.asWebviewUri(
-						vscode.Uri.joinPath(uri,'videos',signLanguage,'languages',editorLanguage,'code',sign.file+'.mp4')),
-					info: sign.info ?
-						webview.asWebviewUri(
-							vscode.Uri.joinPath(uri,'videos',signLanguage,'languages',editorLanguage,'info',sign.info+'.mp4'))
-						: undefined,
-					examples: sign.examples,
-				});
-			}
-
-			categories.push({
-				title: result.title,
-				id: result.id,
-				icon: result.icon,
-				tooltip: webview.asWebviewUri(
-					vscode.Uri.joinPath(uri,'videos',signLanguage,'languages',editorLanguage,'category',result.tooltip+'.mp4')),
-				videos: categoryVideos,
+			tooltips.push({
+				id: tooltip,
+				file: this.webview.asWebviewUri(
+					vscode.Uri.joinPath(this.uri,'videos',this.signLanguage,'tooltip',tooltip+'.mp4')
+				),
 			});
 		}
+		return tooltips;
 	}
 
-	return categories;
-}
-
-function sanitizeEditorText(editor: vscode.TextEditor): string
-{
-	let text: string = '';
-
-	if (editor.selections.length > 0)
+	getError(message: string): ISignVideos[]
 	{
-		for (let selection of editor.selections)
-		{
-			if (text.length > 0) { text += ' '; }
-			const range: vscode.Range = new vscode.Range(selection.start, selection.end);
-			text += editor.document.getText(range);
-		}
+		const videos: ISignVideos[] = [];
+		videos.push({
+			token: 'Erro!',
+			file: this.webview.asWebviewUri(
+				vscode.Uri.joinPath(this.uri,'videos',this.signLanguage,'error',message+'.mp4')
+			),
+			info: undefined,
+			examples: undefined,
+		});
+		return videos;
 	}
-	else { text = editor.document.getText(); }
 
-	text = text.trim().replace(/[\t\v\f ]+/g, ' ').normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+	readCode(editor: vscode.TextEditor): ISignVideos[]
+	{
+		const language: string = editor.document.languageId;
+		const results: ISign[] = [];
+		const videos: ISignVideos[] = [];
+	
+		const text: string = sanitizeEditorText(editor);
+	
+		switch (language)
+		{
+			case 'javascript':
+				results.push(...javascriptCodeChecker.getResults(text));
+				break;
+			default:
+				return videos;
+		}
+	
+		if (results)
+		{
+			for (let result of results)
+			{
+				if (result.isCode)
+				{
+					videos.push({
+						token: result.token,
+						file: this.webview.asWebviewUri(
+							vscode.Uri.joinPath(this.uri,'videos',this.signLanguage,'languages',language,'code',result.file+'.mp4')
+						),
+						info: result.info ?
+							this.webview.asWebviewUri(
+								vscode.Uri.joinPath(this.uri,'videos',this.signLanguage,'languages',language,'info',result.info+'.mp4')
+							) : undefined,
+						examples: undefined,
+					});
+				}
+				else
+				{
+					videos.push({
+						token: result.token,
+						file: this.webview.asWebviewUri(
+							vscode.Uri.joinPath(this.uri,'videos',this.signLanguage,'misc',result.token+'.mp4')
+						),
+						info: undefined,
+						examples: undefined,
+					});
+				}
+			}
+		}
+	
+		return videos;
+	}
 
-	return text;
+	writeCode(editor: vscode.TextEditor, text: string): void
+	{
+		const position: vscode.Position = editor.selection.active;
+		editor.edit((builder: vscode.TextEditorEdit) => { builder.insert(position, text); });
+	}
+
+	getCategories(currentLanguage: string, editorLanguage: string): ICategoryVideos[]
+	{
+		const categories: ICategoryVideos[] = [];
+		
+		if (currentLanguage === editorLanguage) {
+			return categories;
+		}
+		
+		const results: ICategory[] = [];
+	
+		switch (editorLanguage)
+		{
+			case 'javascript':
+				results.push(...javascriptCategories.categories);
+				break;
+			default:
+				return categories;
+		}
+	
+		if (results)
+		{
+			for (let result of results)
+			{
+				const categoryVideos: ISignVideos[] = [];
+				
+				for (let sign of result.signs)
+				{
+					categoryVideos.push({
+						token: sign.token,
+						file: this.webview.asWebviewUri(
+							vscode.Uri.joinPath(this.uri,'videos',this.signLanguage,'languages',editorLanguage,'code',sign.file+'.mp4')
+						),
+						info: sign.info ?
+							this.webview.asWebviewUri(
+								vscode.Uri.joinPath(this.uri,'videos',this.signLanguage,'languages',editorLanguage,'info',sign.info+'.mp4')
+							) : undefined,
+						examples: sign.examples,
+					});
+				}
+	
+				categories.push({
+					title: result.title,
+					id: result.id,
+					icon: result.icon,
+					tooltip: this.webview.asWebviewUri(
+						vscode.Uri.joinPath(this.uri,'videos',this.signLanguage,'languages',editorLanguage,'category',result.tooltip+'.mp4')
+					),
+					videos: categoryVideos,
+				});
+			}
+		}
+	
+		return categories;
+	}
+
+	#sanitizeEditorText(editor: vscode.TextEditor): string
+	{
+		let text: string = '';
+	
+		if (editor.selections.length > 0)
+		{
+			for (let selection of editor.selections)
+			{
+				if (text.length > 0) { text += ' '; }
+				const range: vscode.Range = new vscode.Range(selection.start, selection.end);
+				text += editor.document.getText(range);
+			}
+		}
+		else { text = editor.document.getText(); }
+	
+		text = text.trim().replace(/[\t\v\f ]+/g, ' ').normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+	
+		return text;
+	}
 }
